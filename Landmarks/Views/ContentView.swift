86bcdbd7964a3b01035e87f619c5d4c2fd1e516c
Bryan <71095749+bryanless@users.noreply.github.com>
2022-05-17 08:19:17 +0700

    Fix label

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
             
             LandmarkList()
                 .tabItem {
-                    Label("Featured", systemImage: "list.bullet")
+                    Label("Landmarks", systemImage: "list.bullet")
                 }
         }
     }
